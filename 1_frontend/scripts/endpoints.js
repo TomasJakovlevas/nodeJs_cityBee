fb665482c54d763b526a20fa599bde574cf16a32
TomasJakovlevas <tomasjakovlevas@gmail.com>
2021-08-13 15:18:03 +0300

    first commit

---
+++ 1_frontend/scripts/endpoints.js
@@ -0,0 +1,6 @@
+const host = 'https://nodejs-citybee-atsiskaitymas.herokuapp.com';
+
+export const ALL_MODELS_URI = `${host}/models/`;
+export const GET_ALL_MODELS_WITH_COUNT_URI = `${host}/modelscount/`;
+export const GET_ALL_VEHICLES_URI = `${host}/vehicles/`;
+export const ALL_VEHICLES_URI = `${host}/vehicles/`;
---
+++ 1_frontend/scripts/viewModels.js
@@ -0,0 +1,74 @@
+// Imports
+import { ALL_MODELS_URI } from './endpoints.js';
+import { GET_ALL_MODELS_WITH_COUNT_URI } from './endpoints.js';
+
+// Variables
+// --DOMelements
+const modelTable = document.querySelector('#modelSection table');
+const modelCountTable = document.querySelector('#modelcountSections table');
+
+// Functions
+// Load all tables
+const renderTables = () => {
+  allModels();
+  allModelsWithCount();
+};
+
+// getting all models
+const allModels = () => {
+  return fetch(ALL_MODELS_URI)
+    .then((response) => response.json())
+    .then((result) => {
+      let data = result;
+
+      modelTable.innerHTML = `<thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Model</th>
+        <th scope="col">Hour Price</th>
+      </tr>
+    </thead>`;
+
+      modelTable.innerHTML += data.reduce((total, current) => {
+        return (total += `
+        <tr>
+        <th scope="row">${current._id}</th>
+        <td>${current.name}</td>
+        <td>${current.hour_price.toFixed(2)} €</td>
+      </tr>
+        `);
+      }, '');
+    });
+};
+
+// getting all models with count
+const allModelsWithCount = () => {
+  return fetch(GET_ALL_MODELS_WITH_COUNT_URI)
+    .then((response) => response.json())
+    .then((result) => {
+      let data = result;
+      modelCountTable.innerHTML = `<thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Model</th>
+        <th scope="col">Hour Price</th>
+        <th scope="col">Count</th>
+      </tr>
+    </thead>`;
+
+      modelCountTable.innerHTML += data.reduce((total, current) => {
+        return (total += `
+        <tr>
+        <th scope="row">${current._id}</th>
+        <td>${current.name}</td>
+        <td>${current.hour_price.toFixed(2)} €</td>
+        <td>${current.count}</td>
+
+      </tr>
+        `);
+      }, '');
+    });
+};
+
+// Events
+document.addEventListener('DOMContentLoaded', renderTables);
